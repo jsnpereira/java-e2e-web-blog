d1320e74dce71819b0e6bc662a6c3bccd0e48309
Jeison Da silva Pereira <jeisonpereira@MacBook-Pro-de-Jeison.local>
2026-05-12 20:57:48 -0300

    Adicionado github action workflow -CI/CD

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,15 +3,13 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.AfterSuite;
 
-
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "steps",
-        plugin = {"pretty", "html:target/cucumber-reports"},
+        plugin = {"pretty", "html:target/cucumber-reports/relatorio.html"},
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
-
 
     @AfterSuite
     public void tearDown() {
