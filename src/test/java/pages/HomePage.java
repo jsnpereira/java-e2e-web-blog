34331adf8ea592dfc69a63e3ca744b765210ce4a
Jeison Da silva Pereira <jeisonpereira@MacBook-Pro-de-Jeison.local>
2026-05-12 21:23:14 -0300

    alterar o tempo do timeout da página home page

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -23,7 +23,7 @@ public class HomePage {
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(60));
         this.actions = new Actions(driver);
     }
 
